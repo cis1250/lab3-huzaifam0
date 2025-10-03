119d603639ccfe97f96400cbbb4498cd513e67e3
huzaifam0 <hmemon01@uoguelph.ca>
2025-10-03 12:02:24 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,20 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+user_input = input("How many terms of the Fibonacci sequence do you want?")
+input = int(user_input)
+print("User Input: ", input)
+
+# variables for Fibonacci Sequence
+a = 0
+b = 1
+
+if input >= 0:
+  for i in range(input): 
+    print(a, end=' ')
+    temp = a + b
+    a = b
+    b = temp
+else: 
+  print("Expected output: Please enter a positive integer.")
